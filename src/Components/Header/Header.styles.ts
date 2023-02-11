3e2a61167b41810ae2c456ddc1d408fffb220caa
gattihub <flavi503@hotmail.com>
2023-02-11 12:45:52 -0300

    feat: img style: header

--- src/Components/Header/Header.styles.ts
+++ src/Components/Header/Header.styles.ts
@@ -3,12 +3,21 @@ import styled from 'styled-components';
 export const HeaderDiv = styled.div`
 display:flex;
 width: 100%;
-height: 59px;
-background-color: ${props => props.theme.colors.Primary};
+
+background-color: ${props => props.theme.colors.White};
 margin:0px;
+text-align: center;
+align-items:center;
+`
+
+export const MainImage = styled.img`
+width: 6%;
 `
 
 export const HeaderLinks = styled.a`
-color: ${props => props.theme.colors.White};
+color: ${props => props.theme.colors.Primary};
 text-decoration:none;
+margin: 0px 5px;
+text-align:center;
+font-size: 0.8em;
 `
--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import * as FC from './Header.styles'
+import logoGeek from '../../../Images/imageLogo.png'
+
 
 
 export const Header: React.FC = () => {
         return (
-                <FC.HeaderDiv>                   
-                    <FC.HeaderLinks href="#">Hello, world</FC.HeaderLinks>
+                <FC.HeaderDiv>     
+                    <FC.MainImage src={logoGeek}></FC.MainImage>                  
+                    <FC.HeaderLinks href="#">Shop</FC.HeaderLinks>
+                    <FC.HeaderLinks href="#">Stories</FC.HeaderLinks>
+                    <FC.HeaderLinks href="#">About</FC.HeaderLinks>
                 </FC.HeaderDiv>
         )
 
