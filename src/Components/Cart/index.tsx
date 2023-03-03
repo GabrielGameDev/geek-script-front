97a007ae895d9eac5a6a8eb3e100c75cc2ca95a7
GabrielGameDev <gabrielgamedev@gmail.com>
2023-03-03 18:56:03 -0300

    Adicionar ao carrinho

--- src/Components/Cart/index.tsx
+++ src/Components/Cart/index.tsx
@@ -1,38 +1,85 @@
 import React from "react";
-import * as FC from './Cart.styles'
-
+import * as FC from "./Cart.styles";
+import { getProduct } from "../../api/produtos";
+import { getCategories } from "../../api/category";
+import { useEffect, useState } from "react";
+import { number } from "prop-types";
 
 export const CartContent: React.FC = () => {
-    return (
-        <FC.Grid>
-            <FC.ContentBox gridArea="a" gridFractionRows="1fr 1fr 2fr">
-                <FC.StyledH3>Your Cart</FC.StyledH3>
-                <FC.StyledP>Not ready to checkout? <a>continue shopping</a></FC.StyledP>
-                <FC.ContentBox gridFractionColumns="0.5fr 1fr" >
-                    <FC.StyledImg />
-                    <FC.ContentBox gridFractionRows="1fr 1fr 1fr 1fr" height="70px" justifyContent="left">
-                        <FC.StyledH5>Nome do produto</FC.StyledH5>
-                        <FC.StyledP>categoria</FC.StyledP>
-                        <FC.StyledP>Quantidade: </FC.StyledP>
-                        <FC.StyledH5>Preço</FC.StyledH5>
-                    </FC.ContentBox>
+  const [products, setProducts] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [totalPrice, setTotalPrice] = useState(0);
 
-                </FC.ContentBox>
-            </FC.ContentBox>
-            <FC.ContentBox gridArea="b">
-                <FC.StyledForm>
-                    <FC.StyledH4>Resumo do pedido</FC.StyledH4>
-                    <FC.StyledInput placeholder="Cupom de desconto"></FC.StyledInput>
-                    <FC.ContentBox displayContent="flex" justifyContent="space-between" width="180px"> 
-                        <FC.StyledP>Subtotal</FC.StyledP>
-                        <FC.StyledP>R$00,00</FC.StyledP>
-                    </FC.ContentBox>
-                    <FC.StyledP>Frete</FC.StyledP>
-                    <FC.StyledP>Total</FC.StyledP>
-                    <FC.StyledButton href="http://localhost:5173/checkout">Seguir para o checkout</FC.StyledButton>
-                </FC.StyledForm>
-            </FC.ContentBox>
-        </FC.Grid>
+  function getCategoryById(id: number) {
+    return categories.find((category) => category.id_category === id);
+  }
 
-    )
-}
+  useEffect(() => {
+    getCategories().then((response) => {
+      setCategories(response.data);
+    });
+
+    const cartItems = localStorage.getItem("cart");
+    let cart = [];
+    if (cartItems) {
+      cart = JSON.parse(cartItems);
+    }
+    let total = 0;
+    cart.forEach((id: any) => {
+      getProduct(id).then((response) => {
+        // console.log(response.data);
+        setProducts((products) => [...products, response.data]);
+        total += parseFloat(response.data.price);
+        console.log(total);
+        setTotalPrice(total);
+      });
+    });
+  }, []);
+
+  return (
+    <FC.Grid>
+      <FC.ContentBox gridArea="a" gridFractionRows="1fr 1fr 2fr">
+        <FC.StyledH3>Your Cart</FC.StyledH3>
+        <FC.StyledP>
+          Not ready to checkout? <a>continue shopping</a>
+        </FC.StyledP>
+        {products.map((product: any) => (
+          <FC.ContentBox gridFractionColumns="0.5fr 1fr">
+            <FC.StyledImg src={product.photo} />
+            <FC.ContentBox
+              gridFractionRows="1fr 1fr 1fr 1fr"
+              height="70px"
+              justifyContent="left"
+            >
+              <FC.StyledH5>{product.name}</FC.StyledH5>
+              <FC.StyledP>
+                {getCategoryById(product.category_id).name}
+              </FC.StyledP>
+              {/* <FC.StyledP>Quantidade: </FC.StyledP> */}
+              <FC.StyledH5>Preço:{product.price}</FC.StyledH5>
+            </FC.ContentBox>
+          </FC.ContentBox>
+        ))}
+      </FC.ContentBox>
+      <FC.ContentBox gridArea="b">
+        <FC.StyledForm>
+          <FC.StyledH4>Resumo do pedido</FC.StyledH4>
+          <FC.StyledInput placeholder="Cupom de desconto"></FC.StyledInput>
+          <FC.ContentBox
+            displayContent="flex"
+            justifyContent="space-between"
+            width="180px"
+          >
+            <FC.StyledP>Subtotal</FC.StyledP>
+            <FC.StyledP>R${totalPrice}</FC.StyledP>
+          </FC.ContentBox>
+          <FC.StyledP>Frete: Grátis</FC.StyledP>
+          <FC.StyledP>Total: R${totalPrice}</FC.StyledP>
+          <FC.StyledButton href="http://localhost:5173/checkout">
+            Seguir para o checkout
+          </FC.StyledButton>
+        </FC.StyledForm>
+      </FC.ContentBox>
+    </FC.Grid>
+  );
+};
